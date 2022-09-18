02ad6a954216311479bfaf52f04d21c7b987afb5
Daniel Nikkari <danielnikkari@Daniels-MacBook-Air.local>
2022-09-18 11:56:12 +0300

    animation for the navbar fix v3

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -38,62 +38,54 @@ export const NavBar = () => {
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
-          <div className="animate__animated animate__slideInDown">
-            <Nav className="me-auto">
-              <Nav.Link
-                href="#skills"
-                className={
-                  activeLink === "Skills" ? "active-navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("skills")}
-              >
-                Skills
-              </Nav.Link>
-              <Nav.Link
-                href="#projects"
-                className={
-                  activeLink === "Projects"
-                    ? "active-navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("projects")}
-              >
-                Projects
-              </Nav.Link>
-              <Nav.Link
-                href="#studies"
-                className={
-                  activeLink === "Studies"
-                    ? "active-navbar-link"
-                    : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("studies")}
-              >
-                Studies
-              </Nav.Link>
-              <Nav.Link
-                href="#work"
-                className={
-                  activeLink === "Work" ? "active-navbar-link" : "navbar-link"
-                }
-                onClick={() => onUpdateActiveLink("work")}
-              >
-                Work
-              </Nav.Link>
-            </Nav>
-          </div>
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "Skills" ? "active-navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "Projects" ? "active-navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="#studies"
+              className={
+                activeLink === "Studies" ? "active-navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("studies")}
+            >
+              Studies
+            </Nav.Link>
+            <Nav.Link
+              href="#work"
+              className={
+                activeLink === "Work" ? "active-navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("work")}
+            >
+              Work
+            </Nav.Link>
+          </Nav>
           <span className="navbar-text">
-            <div className="animate__animated animate__slideInDown">
-              <div className="social-icon">
-                <SocialIcon
-                  className="socialicon"
-                  url="https://www.linkedin.com/in/daniel-nikkari-9a67b9178/"
-                />
-                <SocialIcon
-                  className="socialicon"
-                  url="https://github.com/DanielNikkari"
-                />
-              </div>
+            <div className="social-icon">
+              <SocialIcon
+                className="socialicon"
+                url="https://www.linkedin.com/in/daniel-nikkari-9a67b9178/"
+              />
+              <SocialIcon
+                className="socialicon"
+                url="https://github.com/DanielNikkari"
+              />
             </div>
           </span>
         </Navbar.Collapse>
