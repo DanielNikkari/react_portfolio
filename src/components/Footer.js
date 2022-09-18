0d5b4d819a3a4c17d572a2fec85d1aa3d5c49cdc
Daniel Nikkari <danielnikkari@Daniels-MacBook-Air.local>
2022-09-18 11:25:35 +0300

    Removed animation from studies and added it to the footer message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,28 @@
 import { Col, Container, Row } from "react-bootstrap"
 import logo from "../assets/img/logo-planet.png"
 import { SocialIcon } from "react-social-icons"
+import "animate.css"
+import TrackVisibility from "react-on-screen"
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <div className="thank-you-msg-container">
-          <h1 className="thank-you-msg">Thank you for visiting!</h1>
-        </div>
+        <TrackVisibility>
+          {({ isVisible }) => (
+            <div
+              className={
+                isVisible
+                  ? "animate__animated animate__fadeInLeft"
+                  : "animate__animated animate__fadeOutRight"
+              }
+            >
+              <div className="thank-you-msg-container">
+                <h1 className="thank-you-msg">Thank you for visiting!</h1>
+              </div>
+            </div>
+          )}
+        </TrackVisibility>
         <Row className="align-item-center">
           <Col sm={6}>
             <img className="footer-logo" src={logo} alt="logo" />
--- src/components/Study.js
+++ src/components/Study.js
@@ -1,37 +1,23 @@
 import { Row } from "react-bootstrap"
-import "animate.css"
-import TrackVisibility from "react-on-screen"
 
 export const Study = ({ name, programme, place, time, major, minor }) => {
   return (
     <Row>
-      <TrackVisibility>
-        {({ isVisible }) => (
-          <div
-            className={
-              isVisible
-                ? "animate__animated animate__fadeInLeft"
-                : "animate__animated animate__fadeOutRight"
-            }
-          >
-            <div className="study">
-              <h4>
-                {name}
-                <br />
-                <span className="programme">{programme}</span>
-              </h4>
-              <h5>{place}</h5>
-              <p>
-                {time}
-                <br />
-                {major}
-                <br />
-                {minor}
-              </p>
-            </div>
-          </div>
-        )}
-      </TrackVisibility>
+      <div className="study">
+        <h4>
+          {name}
+          <br />
+          <span className="programme">{programme}</span>
+        </h4>
+        <h5>{place}</h5>
+        <p>
+          {time}
+          <br />
+          {major}
+          <br />
+          {minor}
+        </p>
+      </div>
     </Row>
   )
 }
