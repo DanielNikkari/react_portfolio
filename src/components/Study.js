76ca5e616d8d9becbca56988c5a5473bf2b1c210
Daniel Nikkari <danielnikkari@Daniels-MacBook-Air.local>
2022-09-18 11:03:03 +0300

    Added bounce animation to the studies section

--- src/components/Study.js
+++ src/components/Study.js
@@ -1,23 +1,37 @@
 import { Row } from "react-bootstrap"
+import "animate.css"
+import TrackVisibility from "react-on-screen"
 
 export const Study = ({ name, programme, place, time, major, minor }) => {
   return (
     <Row>
-      <div className="study">
-        <h4>
-          {name}
-          <br />
-          <span className="programme">{programme}</span>
-        </h4>
-        <h5>{place}</h5>
-        <p>
-          {time}
-          <br />
-          {major}
-          <br />
-          {minor}
-        </p>
-      </div>
+      <TrackVisibility>
+        {({ isVisible }) => (
+          <div
+            className={
+              isVisible
+                ? "animate__animated animate__fadeInDown"
+                : "animate__animated animate__fadeOutDown"
+            }
+          >
+            <div className="study">
+              <h4>
+                {name}
+                <br />
+                <span className="programme">{programme}</span>
+              </h4>
+              <h5>{place}</h5>
+              <p>
+                {time}
+                <br />
+                {major}
+                <br />
+                {minor}
+              </p>
+            </div>
+          </div>
+        )}
+      </TrackVisibility>
     </Row>
   )
 }
