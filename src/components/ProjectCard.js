77d48bffe5167adb666ac3d560509cd108852693
Daniel Nikkari <danielnikkari@dhcp-101-113.eduroam.aalto.fi>
2022-10-24 11:57:22 +0300

    removed clickability from projects wihtout url path

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({ title, description, imgUrl, url }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <a id="proj-url" href={url} target="_blank" rel="noreferrer">
+        <a id="proj-url" href={url} target="_blank" rel="noreferrer" className={url === "" ? "disablelink" : ""}>
           <img src={imgUrl} alt="project" />
           <div className="proj-txtx">
             <h4>{title}</h4>
